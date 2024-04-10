96a9bb2b5c9ec03e6ef9a7874212268292969944
Daria Efimova <agaralambinia@yandex.ru>
2024-04-10 19:40:40 +0400

    v1

--- incs/minitalk_bonus.h
+++ incs/minitalk_bonus.h
@@ -1,6 +1,9 @@
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
 
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
 # include <stdio.h>
 # include <sys/types.h>
 # include <limits.h>
